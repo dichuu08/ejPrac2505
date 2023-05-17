19eb02c12c8d303c8c6d915de826fbef9dbf4b38
DBeewms <diego.mora16063@est.uca.edu.ni>
2023-05-17 08:05:14 -0600

    Se agrega la funcion pedirNumeros

--- EjercicioIF.cpp
+++ EjercicioIF.cpp
@@ -12,3 +12,14 @@ int numeroMayor(int num1, int num2){
     cout << "Ambos numeros son iguales " << endl;
     return 0;
 }
+
+void pedirNumeros(){
+    int num1, num2;
+
+    cout << "Dime un numero: ";
+    cin >> num1;
+    cout << "Dime otro numero: ";
+    cin >> num2;
+
+    cout << "El numero mayor es: " << numeroMayor(num1, num2) << endl;
+}
